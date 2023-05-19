61b8f05fa2db52eea14b1898249eccc8d840ff22
yseojun <ddt1016@naver.com>
2023-05-19 12:38:39 +0900

    save

--- project/main.c
+++ project/main.c
@@ -2,13 +2,17 @@
 #include "SEG_MODULE.h"
 #include <avr/io.h>
 #include <avr/delay.h>
-#include <string.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
 volatile unsigned int cnt;
 volatile unsigned int timer;
-volatile unsigned int input;
+
+volatile unsigned int play_game;
+volatile unsigned int level;
+volatile unsigned int input[10];
+volatile unsigned int expect[10];
+volatile unsigned int now_input;
 
 void	init_all()
 {
@@ -18,12 +22,7 @@ void	init_all()
 	PORTC = 0x0F;  // PC0~PC3 initially output high
 	DDRC = 0x0F;   // PC0~PC3 are assigned to output pins
 
-	UCSR1A = 0x00; // USART initialization
-	UCSR1B = 0x98; // rx, tx enable + Rx complete Interrupt enable
-	UCSR1C = 0x06; // No parity, 1 stop bit, 8 data bit frame
-	UBRR1H = 0x00;  // USART mode :  Asynchronous (without XCLK)
-	UBRR1L = BAUD_9600;
-
+	PORTD = 0;
 	MCUCR = 0x00;
 	EICRB = 0xFF;
 	EIMSK = 1 << INT7 | 1 << INT6 | 1 << INT5 | 1 << INT4;
@@ -43,6 +42,7 @@ ISR(TIMER0_OVF_vect)
 	cnt++;
 	if (cnt == 100)
 	{
+		PORTG ^= 1 << PG0;
 		cnt = 0;
 		timer++;
 	}
@@ -50,52 +50,118 @@ ISR(TIMER0_OVF_vect)
 
 ISR(INT4_vect)
 {
-	tx_string("Left\n");
+	
 }
 
 ISR(INT5_vect)
 {
-	tx_string("Down\n");
+	now_input = 1;
 }
 
 ISR(INT6_vect)
 {
-	tx_string("Up\n");
+	now_input = 2;
 }
 
 ISR(INT7_vect)
+{	
+	now_input = 3;
+}
+
+void	chk_level()
 {
-	tx_string("Right\n");
+	int	idx = 0;
+	while (idx < level)
+	{
+		if (input[idx] != expect[idx])
+		{
+			tx_string("Fail\n");
+			_delay_ms(1000);
+			play_game = 0;
+			level = 1;
+		}
+		idx++;
+	}
+	tx_string("Level Done!!!\n");
+	level++;
+	if (level == 10)
+	{
+		tx_string("You Win!!!\n");
+		_delay_ms(1000);
+		play_game = 0;
+		level = 1;
+	}
 }
 
 void	do_level()
 {
-	while (1)
+	tx_string("Remember !!\n");
+	int	random;
+	int idx = 0;
+	while (idx < level)
 	{
-		tx_string("doing game\n");
-		_delay_ms(1000);
+		random = rand() % 3 + 1;
+		if (random == 1)
+			tx_string("Left!!!\n");
+		else if (random == 2)
+			tx_string("Up!!\n");
+		else if (random == 3)
+			tx_string("Right!\n");
+		expect[idx] = random;
+		idx++;
+		_delay_ms(500);
 	}
+	tx_string("Remember!!\n");
+	_delay_ms(1000);
+	for (int i = 0; i < 20; i++)
+		tx_string("\n");
+	tx_string("Start Game!!\n");
+	tx_string("Taking input...\n");
+	idx = 0;
+	while (idx < level)
+	{
+		now_input = 0;
+		while (now_input == 0);
+		if (now_input == 1)
+			tx_string("Left\n");
+		else if (now_input == 2)
+			tx_string("Up\n");
+		else if (now_input == 3)
+			tx_string("Right\n");
+		input[idx] = now_input;
+		idx++;
+	}
+	chk_level();
 }
 
 int main(void)
 {
 	init_all();
-	input = 0;
+	usart_init(BAUD_9600);
+	play_game = 0;
+	level = 1;
 	while (1)
 	{
-		if (playing_game == 1)
+		tx_string("In main\n");
+		if (play_game == 1)
 		{
+			if (level == 1)
+			{
+				tx_string("Left : 5, Up : 6, Right : 7\n");
+				tx_string("Remember script, and press in order!\n");
+			}
 			do_level();
-			// chk_input();
 		}
 		else
 		{
-			tx_string("Press PG3 to Start!");
-			while (1)
+			while (play_game == 0)
 			{
-				if (PING && PG3)
-					break ;
+				tx_string("Press PG3 to start GAME!\n");
+				if (PING & (1 << PG3))
+					play_game = 1;
+				_delay_ms(1000);
 			}
 		}
+		_delay_ms(100);
 	}
 }
